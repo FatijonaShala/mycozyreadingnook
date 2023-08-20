9689a28e9900abf17be6c8d15739676fe64b1341
Fatjona <fatjonaash1@gmail.com>
2023-08-20 17:35:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Homepage from "./homepage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
