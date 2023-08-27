e8d5bc790945d55fcff6be2330cd0cacd3beb8e9
Fatjona <fatjonaash1@gmail.com>
2023-08-27 02:17:42 +0200

    Merge remote changes

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -19,18 +19,17 @@ function Homepage() {
               <img
                 src="images/nook1upd.png"   
                 alt="Illustration of a reading nook"
-                className="img-fluid centered-image"
-                
+                className="img-fluid centered-image"    
               />
             </section>
           </Col>
           <Col>
             <section className="content-section">
-              <AmbiancePlayer src="/audio/rain.mp3" title="rainy day" />          
-              <AmbiancePlayer src="/audio/sunny.mp3" title="sunny day" />
+               <AmbiancePlayer src="/audio/rain.mp3" title="rainy day" />           
+               <AmbiancePlayer src="/audio/sunny.mp3" title="sunny day" />
                <AmbiancePlayer src="/audio/turning-pages.mp3" title="turning pages" />
               <AmbiancePlayer src="/audio/fireplace.mp3" title="fireplace crackling" />
-             <AmbiancePlayer src="/audio/typing-keyboard.mp3" title="typing on keyboard" />
+             <AmbiancePlayer src="/audio/typing-keyboard.mp3" title="typing on keyboard" /> 
             </section>
           </Col>
         </Row>
